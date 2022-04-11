0e500516dbb5d8b70f3ebf5b4a8238a6aa5eb953
DoDucHieu <hieuncncnc@gmail.com>
2022-04-11 20:38:27 +0700

    fix design interface

--- src/components/CourseList/CourseItem.tsx
+++ src/components/CourseList/CourseItem.tsx
@@ -6,7 +6,7 @@ import CardContent from '@mui/material/CardContent'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import { Grid } from '@mui/material'
-import { flexbox } from '@mui/system'
+import { flexbox, width } from '@mui/system'
 import Stack from '@mui/material/Stack'
 
 const bull = (
@@ -22,7 +22,6 @@ export default function CourseItem({}: Props) {
       sx={{
         flex: '1 0 0',
         width: '50%',
-        height: '500px',
         px: '80px',
         m: '20px',
         display: 'flex',
@@ -31,9 +30,10 @@ export default function CourseItem({}: Props) {
         background: 'pink',
         borderRadius: '10px',
         fontFamily: 'Roboto Slab',
+        pb: '80px',
       }}
     >
-      <h2>Lớp đang diễn ra</h2>
+      <h2 className="my-5">Lớp đang diễn ra</h2>
       <Card
         sx={{
           width: '100%',
@@ -46,14 +46,26 @@ export default function CourseItem({}: Props) {
             alt=""
             className="w-3/4 h-1/2 object-cover rounded-lg pb-2 "
           />
-          <Typography variant="h5" component="div" className="font">
+          <Typography variant="h5" component="div" className="font w-full pl-14 mt-4">
             Lập trình web bằng PHP
           </Typography>
-          <Typography variant="body2">Giáo viên: Hoàng Ngọc Hậu</Typography>
-          <Typography variant="body2">Note: Hôm nay chúng ta nghỉ!</Typography>
-          <Typography variant="body2">Thời gian học</Typography>
-          <Typography className="border-t border-current"></Typography>
-          <Stack spacing={2} direction="row" className="w-full">
+          <Typography variant="body2" className="font w-full  pl-14 mt-4" sx={{ fontWeight: '600' }}>
+            Giáo viên: Hoàng Ngọc Hậu
+          </Typography>
+          <Typography variant="body2" className="font w-full  pl-14 mt-4">
+            Note: Hôm nay chúng ta nghỉ!
+          </Typography>
+          <Typography
+            sx={{ height: '1px', width: '100%', background: '#DADDE4' }}
+            className="font w-full mt-4"
+          ></Typography>
+          <Typography variant="body2" className="font w-full  pl-14 mt-4" sx={{ fontWeight: '600' }}>
+            Thời gian học
+          </Typography>
+          <Typography variant="body2" className="font w-full  pl-14 mt-2">
+            5h30PM - 7h30PM
+          </Typography>
+          <Stack spacing={2} direction="row" className="w-full px-14">
             <Button variant="contained" className="mt-4 w-full">
               Vào học
             </Button>
