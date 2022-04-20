0088902b031e2d69cb7e3d129ede0263ee8994fe
vungocduyet24 <vungocduyet24@gmail.com>
2022-04-20 19:16:19 +0700

    add nesting route admin page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import Layer from './components/Layer'
 import ClassPage from './pages/ClassPage'
 import AdminPage from './pages/AdminPage'
 import HomePage from './pages/HomePage'
+import TableShow from './pages/AdminPage/components/TableShow'
 
 function App() {
   return (
     <Layer>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/classes" element={<ClassPage />} />
-        <Route path="/user-management" element={<AdminPage />} />
+        <Route path="classes" element={<ClassPage />} />
+
+        <Route path="user-management" element={<AdminPage />}>
+          <Route path="teacher" element={<TableShow />} />
+          <Route path="student" element={<TableShow />} />
+        </Route>
       </Routes>
     </Layer>
   )
