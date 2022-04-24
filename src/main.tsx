f795044eb9d7a86e4bffa5f604a0a4e4c8edc727
vungocduyet24 <vungocduyet24@gmail.com>
2022-04-24 22:51:18 +0700

    install and config react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import { ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import theme from './theme'
 
+const queryClient = new QueryClient()
+
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
