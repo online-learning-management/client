a5975f89a2aee5c1e2031d9f870433448b1e2321
DoDucHieu <hieuncncnc@gmail.com>
2022-05-18 00:24:48 +0700

    logic register class

--- src/components/RegisterCourse/NotificationModal.jsx
+++ src/components/RegisterCourse/NotificationModal.jsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
-import ListSubjectToRegister from './ListClassToRegister'
+import ListClassToRegister from './ListClassToRegister'
 
 const style = {
   position: 'absolute',
